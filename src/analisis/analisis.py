551101f47849345564f426139240b5580a911f0c
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2020-12-30 21:01:44 +0100

    Initial commit

---
+++ src/analisis/analisis.py
@@ -0,0 +1,14 @@
+import
+
+def getMonthExpenses(data, month):
+    #TODO
+
+def getTotalMonthExpenses(data, month):
+    #TODO
+
+def getMonthIncome(data, month):
+    #TODO
+
+def getTotalMonthIncome(data, month):
+    #TODO
+
---
+++ src/extract/extract.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def getData(dateInit, dateEnd):
+    path = "../../data/listado_de_movimientos.csv"
+    df = pd.read_csv(path, delimiter=';')
+    return cleanData(df)
+
+def cleanData(data):
+    data.pop("Unnamed: 0")
+    data.pop("Unnamed: 6")
+    return df
