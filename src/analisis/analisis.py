8c39ddb7884b828a81a00458e1645e5b243e855e
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2021-01-02 00:07:52 +0100

    New month expenses and income functions

--- src/analisis/analisis.py
+++ src/analisis/analisis.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import datetime
 
-def getMonthExpenses(data, month):
+def getMonthExpenses(data, month, year=datetime.datetime.utcnow().year):
     df = getData()
-    year = datetime.datetime.utcnow().year
+    df = df[df['month'] == month & df['year'] == year]
+    return df[df['Importe']<0]['Importe'].sum()
+
+def getMonthIncome(data, month, year=datetime.datetime.utcnow().year):
+    df = getData()
+    df = df[df['month'] == month & df['year'] == year]
+    return df[df['Importe']>0]['Importe'].sum()
+
+def getTotalOfMonth(data, month, year=datetime.datetime.utcnow().year):
+    df = getData()
     df = df[df['month'] == month & df['year'] == year]
     return df['Importe'].sum()
 
-def getTotalMonthExpenses(data, month):
-    #TODO
-
-def getMonthIncome(data, month):
-    #TODO
-
-def getTotalMonthIncome(data, month):
-    #TODO
 
