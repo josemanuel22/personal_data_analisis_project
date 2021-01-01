0e33281ffd135a3426d54610a0a31e1cd4386a08
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2021-01-01 20:56:43 +0100

    Adding timestamp filtering

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -3,9 +3,9 @@ import pandas as pd
 def getData(dateInit, dateEnd):
     path = "../../data/listado_de_movimientos.csv"
     df = pd.read_csv(path, delimiter=';')
-    #ADD dateInit, dateEnd parsing
-    return cleanData(df)
-
+    cleanData(df)
+    df = df[df['Fecha'] >= dateInit & df['Fecha']<= dateEnd]
+    return
 def cleanData(data):
     data.pop("Unnamed: 0")
     data.pop("Unnamed: 6")
