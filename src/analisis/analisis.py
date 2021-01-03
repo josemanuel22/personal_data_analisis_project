0bf4f9c34fdfd02e1701d55aac2dcbd4151f08e5
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2021-01-03 01:20:31 +0100

    Add function to categorize data

--- src/analisis/analisis.py
+++ src/analisis/analisis.py
@@ -2,18 +2,20 @@ import pandas as pd
 import datetime
 
 def getMonthExpenses(data, month, year=datetime.datetime.utcnow().year):
-    df = getData()
-    df = df[df['month'] == month & df['year'] == year]
-    return df[df['Importe']<0]['Importe'].sum()
+    data = data[data['month'] == month & data['year'] == year]
+    return data[data['Importe']<0]['Importe'].sum()
 
 def getMonthIncome(data, month, year=datetime.datetime.utcnow().year):
-    df = getData()
-    df = df[df['month'] == month & df['year'] == year]
-    return df[df['Importe']>0]['Importe'].sum()
+    data = data[data['month'] == month & data['year'] == year]
+    return data[data['Importe']>0]['Importe'].sum()
 
 def getTotalOfMonth(data, month, year=datetime.datetime.utcnow().year):
-    df = getData()
-    df = df[df['month'] == month & df['year'] == year]
-    return df['Importe'].sum()
+    data = data[data['month'] == month & data['year'] == year]
+    return data['Importe'].sum()
 
-
+def categorizeData(data, month, year=datetime.datetime.utcnow().year):
+    data = data[data['month'] == month & data['year'] == year]
+    df.groupby(['Concepto']).sum()
+    for category in categories:
+        for categoryStr in category:
+            df[df['Concepto'].str.contains(categoryStr)].sum()
