dd0b5c79c63e445637833f4dcddcbb8e42a235fb
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2021-01-19 00:50:34 +0100

    Adding float filtering

--- src/analisis/analisis.py
+++ src/analisis/analisis.py
@@ -5,15 +5,15 @@ import pandas as pd
 import datetime
 
 def getMonthExpenses(data, month, year=datetime.datetime.utcnow().year):
-    data = data[data['month'] == month & data['year'] == year]
+    data = data[data['month'] == month][data['year'] == year]
     return data[data['Importe']<0]['Importe'].sum()
 
 def getMonthIncome(data, month, year=datetime.datetime.utcnow().year):
-    data = data[data['month'] == month & data['year'] == year]
+    data = data[data['month'] == month][data['year'] == year]
     return data[data['Importe']>0]['Importe'].sum()
 
 def getTotalOfMonth(data, month, year=datetime.datetime.utcnow().year):
-    data = data[data['month'] == month & data['year'] == year]
+    data = data[data['month'] == month][data['year'] == year]
     return data['Importe'].sum()
 
 def categorizeData(data, month, year=datetime.datetime.utcnow().year):
