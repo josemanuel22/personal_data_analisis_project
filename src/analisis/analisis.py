7effb188edf91662dd8e58631a47bd825485b8cc
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2021-01-23 22:00:00 +0100

    sum by category continuation

--- src/analisis/analisis.py
+++ src/analisis/analisis.py
@@ -1,6 +1,5 @@
 import sys
-sys.path.append('../extract')
-
+sys.path.append('/Users/jose/Documents/GitHub/personal_data_analisis_project/src/extract')
 import pandas as pd
 import datetime
 
@@ -16,13 +15,24 @@ def getTotalOfMonth(data, month, year=datetime.datetime.utcnow().year):
     data = data[data['month'] == month][data['year'] == year]
     return data['Importe'].sum()
 
+categories = { "Supermercado" : ["ahorramas", "corte", "dia", "carrefour", "Aldi"],
+            "transporte" : ["renfe", "metro"],
+            "Ocio": ["Glovo", "Amazon", "Mcdonalds", "Ginos", "Rodilla", "Pizzeria", "Starbucks"],
+            "transferencias": ["Transferencia"]
+        }
+
 def categorizeData(data, month, year=datetime.datetime.utcnow().year):
     df = data[data['month'] == month][data['year'] == year]
     df.groupby(['Concepto']).sum()
     dfResults = pd.DataFrame(columns=categories.keys())
     for category in categories:
         res = 0
-        for categoryStr in category:
+        for categoryStr in categories[category]:
             res+=df[df['Concepto'].str.contains(categoryStr)]['Importe'].sum()
         dfResults[category] = res
+    return dfResults
 
+if __name__ == "__main__":
+    from extract import *
+    data = getData()
+    print(categorizeData(data, 4, 2020))
--- src/extract/extract.py
+++ src/extract/extract.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 def getData():
     path = "../../data/listado_de_movimientos.csv"
-    df = pd.read_csv(path, delimiter=';', dtype = {'Importe':float}, decimal=",")
+    data = pd.read_csv(path, delimiter=';', dtype = {'Importe':float}, decimal=",")
     return cleanData(data)
+
 def cleanData(data):
     data.pop("Unnamed: 0")
-    df['year'] = pd.DatetimeIndex(data['Fecha']).year
-    df['month'] = pd.DatetimeIndex(data['Fecha']).month
-    return df
+    data['year'] = pd.DatetimeIndex(data['Fecha']).year
+    data['month'] = pd.DatetimeIndex(data['Fecha']).month
+    return data
